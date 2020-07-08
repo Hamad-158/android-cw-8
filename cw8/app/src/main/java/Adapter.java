7da1540f73ec27c92356ccec100503e4a9bbb347
Hamad-158 <Hamad.bfar@gmail.com>
2020-07-08 18:23:20 +0300

    ما مداني اكمل بس وحشه اخر كلاس ورك

---
+++ cw8/app/src/main/java/Adapter.java
@@ -0,0 +1,35 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cw7.R;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter {
+
+    ArrayList<pokemon>pArry;
+    public MyAdapter(ArrayList <pokemon> pArry ){
+        this.pArry = pArry;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
+        return hv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
